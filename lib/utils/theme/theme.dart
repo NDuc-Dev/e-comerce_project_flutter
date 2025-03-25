650f17722fca775a92de9dbaf26319ac74122ccb
NDuc <nguyenngocduc260504@gmail.com>
2025-03-25 23:39:20 +0700

    'first_commit'

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:e_cormerce_project/utils/theme/custom_theme/app_text_theme.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData()
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: AppTextTheme.darkTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData()
+  );
+}
